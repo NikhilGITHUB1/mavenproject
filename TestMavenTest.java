bdf760c7b648c4479a84d0694e7ddb77298f0bf6
NikhilGITHUB1 <kanojia.nikhil07@gmail.com>
2021-04-14 18:40:00 +0530

    maven code

---
+++ TestMavenTest.java
@@ -0,0 +1,15 @@
+package testmaven;
+
+import org.testng.annotations.Test;
+
+public class TestMavenTest {
+    
+	@Test
+	public void FirstTest() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("maven tested successfully");
+
+	}
+
+}
